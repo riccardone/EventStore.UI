a272432679b0ada63d22ea68472d4bdc1a7fe2f1
pgermishuys <pieter@pieterg.com>
2015-12-03 13:24:11 +0200

    Move regular toast notifications to the top right

--- src/js/services/MessageService.js
+++ src/js/services/MessageService.js
@@ -5,13 +5,15 @@ define(['./_module'], function (app) {
 	return app.factory('MessageService', ['toastr',
 		function (toastr) {
 			function getToastOptions(requiresUserAck, level){
-				return {
+				var options = {
 					closeButton: requiresUserAck ? true : level == 'warning' ? true : false,
-					positionClass: requiresUserAck ? 'toast-container-modal toast-bottom-full-width' : 'toast-bottom-full-width',
+					positionClass: requiresUserAck ? 'toast-container-modal toast-bottom-full-width' : 'toast-top-right',
 					timeOut: requiresUserAck ? 0 : 
 							  level == 'warning' ? 5000 : 
 							  level == 'info' || level == 'success' ? 2000 : 0
 				}
+                console.log(requiresUserAck, level, options);
+                return options;
 			}
 			function getToastTitle(title, level){
 				title = title || level;
@@ -28,6 +30,7 @@ define(['./_module'], function (app) {
 					toastr.warning(text, getToastTitle(title, 'Warning'), getToastOptions(false, 'warning'));
 				},
 				failure: function (text, title) {
+                    toastr.clear();
 					toastr.error(text, getToastTitle(title, 'Failure'), getToastOptions(true, 'failure'));
 				},
 				confirm: function (text) {
